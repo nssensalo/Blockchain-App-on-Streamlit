8c0613e8f119000b290add0b9af2cc032cf576ce
Anna <nssensalo@gmail.com>
2021-11-13 16:30:59 -0800

    Added folder of images

--- Starter_Code/crypto_wallet.py
+++ Starter_Code/crypto_wallet.py
@@ -14,7 +14,9 @@ from web3 import Account
 from web3.auto.infura.kovan import w3
 from web3 import middleware
 from web3.gas_strategies.time_based import medium_gas_price_strategy
-
+import streamlit as st
+from web3 import Web3
+# w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
 ################################################################################
 # Wallet functionality
 
